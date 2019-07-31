1683eb43614777b54ccd03dbed5fda32cba69fe1
Díaz Urbaneja Víctor Eduardo Diex <diaz.victor@openmailbox.org>
2019-07-31 02:10:09 -0400

    added a timer in init.lua

---
+++ init.lua
@@ -0,0 +1,22 @@
+#!/usr/bin/lua
+--[[--
+ @package   Escaarr
+ @filename  init.lua
+ @version   1.0
+ @author    Díaz Urbaneja Víctor Eduardo Diex <diaz.victor@openmailbox.org>
+ @date      23.07.2019 09:51:10 -04
+]]
+
+require("lib.utils")
+
+for h in utils.range(tonumber(os.date("%H")), 24) do
+	for m in utils.range(tonumber(os.date("%M")), 60) do
+		for s in utils.range(tonumber(os.date("%S")), 60) do
+			print("Hora: " .. h .. "Minuto: " .. m .. "Segundo: " .. s)
+			utils.sleep(1000)
+
+			utils.voice("Escaarr, dando el reporte de la hora")
+			utils.voice("son las ".. os.date("%I") .. " con " .. os.date("%M") .. " minutos ")
+		end
+	end
+end
